536ff8006c5ec8a0acc2febc5d9719267319f718
Gabriel Diniz <gabrieldiniz@Gabriels-MacBook-Air.local>
2025-07-12 08:21:50 -0300

    Feat: Jest install

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+    // jest.config.js
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    const nextJest = require('next/jest')
+
+    const createJestConfig = nextJest({
+      dir: './',
+    })
+
+    const customJestConfig = {
+      setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+      moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/$1',
+      },
+      testEnvironment: 'jest-environment-jsdom',
+    }
+
+    module.exports = createJestConfig(customJestConfig)
